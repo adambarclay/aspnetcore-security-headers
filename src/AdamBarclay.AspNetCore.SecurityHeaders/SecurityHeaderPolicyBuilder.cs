36b7649ecb610f7bda64cc6e5e326b907f78cb6d
Adam Barclay <adam@adambarclay.co.uk>
2020-04-30 17:48:26 +0100

    Add referrer-policy configuration

--- src/AdamBarclay.AspNetCore.SecurityHeaders/SecurityHeaderPolicyBuilder.cs
+++ src/AdamBarclay.AspNetCore.SecurityHeaders/SecurityHeaderPolicyBuilder.cs
@@ -6,10 +6,14 @@ namespace AdamBarclay.AspNetCore.SecurityHeaders
 		internal SecurityHeaderPolicyBuilder()
 		{
 			this.FrameOptions = new FrameOptions(this);
+			this.ReferrerPolicy = new ReferrerPolicy(this);
 		}
 
 		/// <summary>Gets the frame options configuration builder.</summary>
 		public FrameOptions FrameOptions { get; }
+
+		/// <summary>Gets the referrer policy configuration builder.</summary>
+		public ReferrerPolicy ReferrerPolicy { get; }
 
 		internal SecurityHeaderPolicy Build()
 		{
@@ -17,7 +21,7 @@ namespace AdamBarclay.AspNetCore.SecurityHeaders
 				"default-src 'self'",
 				"accelerometer 'none';ambient-light-sensor 'none';autoplay 'none';battery 'none';camera 'none';display-capture 'none';document-domain 'none';encrypted-media 'none';fullscreen 'none';geolocation 'none';gyroscope 'none';layout-animations 'none';legacy-image-formats 'none';magnetometer 'none';microphone 'none';midi 'none';oversized-images 'none';payment 'none';picture-in-picture 'none';publickey-credentials 'none';sync-xhr 'none';unsized-media 'none';usb 'none';xr-spatial-tracking 'none';",
 				this.FrameOptions.Build(),
-				"strict-origin-when-cross-origin",
+				this.ReferrerPolicy.Build(),
 				"max-age=31536000;includeSubdomains");
 		}
 	}
